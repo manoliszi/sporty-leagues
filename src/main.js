6836d1126c1fe4033d9e50230dd3977af552143e
manolis <manolis.zifos@gmail.com>
2026-01-30 17:42:18 +0200

    Initial set up. Add vuetify and Vuex.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
